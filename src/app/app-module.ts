610b98ca87855615ba83a142973e3c6dd46246a6
Enzo Adolfo Rubattino Huarzaya <earh1@gcloud.ua.es>
2026-04-21 14:40:10 +0200

    Formulario de acceso o Login

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -9,6 +9,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 // Importamos el nuevo interceptor funcional
 import { AuthInterceptor } from './interceptors/auth-interceptor';
+// Importamos FormsModule para el manejo de formularios en LoginComponent
+import { FormsModule } from '@angular/forms';
 
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -24,13 +26,12 @@ import { TasksComponent } from './components/tasks/tasks.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    // 1. Configuramos el cliente HTTP usando la API funcional
-    // 2. Registramos el interceptor funcional dentro de withInterceptors
     provideHttpClient(
       withInterceptors([AuthInterceptor])
     )
